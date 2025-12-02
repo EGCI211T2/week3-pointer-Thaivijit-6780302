ad2d91149dabc8f366ce957897b90dd4f805ec3d
Thaivijit-6780302 <thaivijit.sid@student.mahidol.edu>
2025-12-02 03:34:22 +0000

    Done

--- main.cpp
+++ main.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 #include <iomanip>
-#define SIZE 10
-
+#include <cstdlib> // for atoi
 using namespace std;
 
-int main(int argc,char *argv[]){
-  int *pa, *pb, i, temp;
-int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
-pa = &a[0]; //pa =a;
-cout<<"Original: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
-}
-cout<<setw(3)<<*pa<<endl;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <list of numbers>" << endl;
+        return 1;
+    }
 
-pa = &a[0];  
-pb = &a[SIZE-1];
-for (i=0; i<SIZE/2; i++)
-{
-   temp = *pa;  
-   *pa = *pb;  
-   *pb = temp;
-   pa++;  pb--;
-}
-pa=&a[0];
-pb-=SIZE/2;
-cout<<"Reversed: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
-}
-cout<<setw(3)<<*pa<<endl;
+    int n = argc - 1;       // number of integers
+    int *pa = new int[n];   // dynamic allocation
 
+    // Fill the array from argv
+    for (int i = 0; i < n; i++) {
+        pa[i] = atoi(argv[i + 1]); // convert string to int
+    }
 
-return 0;
+    // Print original array
+    cout << "Original: ";
+    for (int i = 0; i < n; i++) {
+        cout << setw(3) << pa[i];
+    }
+    cout << endl;
+
+    // Reverse the array
+    for (int i = 0; i < n / 2; i++) {
+        int temp = pa[i];
+        pa[i] = pa[n - 1 - i];
+        pa[n - 1 - i] = temp;
+    }
+
+    // Print reversed array
+    cout << "Reversed: ";
+    for (int i = 0; i < n; i++) {
+        cout << setw(3) << pa[i];
+    }
+    cout << endl;
+
+    // Free allocated memory
+    delete[] pa;
+
+    return 0;
 }
